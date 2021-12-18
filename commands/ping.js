77a4b592bb9861b51185245eb8baf3e8ba8aab46
Han028 <hansatcode@kakao.com>
2021-12-19 01:37:22 +0900

    Replacement of errors that can occur in computational formulas

--- commands/ping.js
+++ commands/ping.js
@@ -3,8 +3,9 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
-		.setDescription('디스코드 서버와 클라이언트 간의 반응 속도를 테스트합니다.'),
+		.setDescription('디스코드 서버와 클라이언트 간의 API 왕복 시간을 측정합니다.'),
 	async execute(interaction) {
-		await interaction.reply({ content: `레이턴시: ${Date.now() - interaction.createdTimestamp}ms` });
+		const sent = await interaction.reply({ content: '(측정중)', fetchReply: true });
+		interaction.editReply(`API 왕복 시간은 ${sent.createdTimestamp - interaction.createdTimestamp}ms 입니다.`);
 	},
 };
