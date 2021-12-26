22900c178d7abfbde4f51cf59afab11d3126a2f7
Han028 <hansatcode@kakao.com>
2021-12-26 23:50:32 +0900

    Modify Chat interface, Command description

--- commands/kon.js
+++ commands/kon.js
@@ -24,21 +24,19 @@ module.exports = {
 		.setDescription('움직이는 이모지를 출력합니다.')
 		.addStringOption(option =>
 			option.setName('name')
-				.setDescription('출력할 이모지 이름을 입력해주세요.')
+				.setDescription('이모지 이름 | 모른다면 아무거나 쳐서 도움말을 보는것도 정답')
 				.setRequired(true)),
 	async execute(interaction) {
+		console.log(interaction.options.getString('name'));
 		if (emojiFiles.includes(`${interaction.options.getString('name')}`)) {
 			const emojiFile = new MessageAttachment(`./src/emoji/${interaction.options.getString('name')}.gif`);
-			const emojifEmbed = new MessageEmbed()
-				.setAuthor(`${interaction.user.username}`, `${interaction.user.avatarURL()}`)
-				.setImage(`attachment://${interaction.options.getString('name')}.gif`);
-			await interaction.reply({ embeds: [emojifEmbed], files: [emojiFile] });
+			await interaction.reply({ files: [emojiFile] });
 		}
 		else {
 			const emojifEmbed = new MessageEmbed()
 				.setColor('#ffcccc')
 				.setTitle(`'${interaction.options.getString('name')}'(을)를 찾을 수 없음`)
-				.setDescription(`이모지가 존재하지 않습니다. (대·소문자 구별 필수)\n사용 가능한 이모지는 다음과 같습니다.\n${emojiFilesName}`);
+				.setDescription(`이모지가 등록되어있지 않습니다. (대·소문자 구별)\n사용 가능한 이모지는 다음과 같습니다. - ${emojiFiles.length}개\n\n${emojiFilesName}`);
 			await interaction.reply({ embeds: [emojifEmbed], ephemeral: true });
 		}
 	},
