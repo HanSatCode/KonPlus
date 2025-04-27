c005b6f6c15097f71eaa8cfb61461356c57c4062
Han028 <han028@yu.ac.kr>
2025-04-27 16:05:15 +0900

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -5,7 +5,7 @@ module.exports = {
 		.setName('ping')
 		.setDescription('디스코드 서버와 클라이언트 간의 API 왕복 시간을 측정합니다.'),
 	async execute(interaction) {
-		const sent = await interaction.reply({ content: '측정중..', fetchReply: true, ephemeral: true });
+		const sent = await interaction.reply({ content: '측정중..', fetchReply: true});
 		interaction.editReply({ content: `API 왕복 시간은 ${sent.createdTimestamp - interaction.createdTimestamp}ms 입니다.`, ephemeral: true });
 	},
 };
